9b0f3700054835955703b1050c6e04e84b894d1e
Ezreal <1649811141@qq.com>
2018-07-05 07:25:38 +0800

    Create Search Insert Position.java

---
+++ Search Insert Position.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        return binarySearch(nums, target, 0, nums.length-1);
+    }
+    
+    public int binarySearch(int[] nums, int target, int left, int right) {
+        int mid = (left + right) / 2;
+        
+        if(left >= right) {
+            if(nums[mid] < target) return mid + 1;
+            else return mid;
+        }
+       
+        if(nums[mid] < target) {
+            binarySearch(nums, target, mid, right);
+        } else if(nums[mid] > target) {
+            binarySearch(nums, target, left, mid);
+        } else {
+            return nums[mid];
+        }
+        
+        return mid;
+    }
+}
