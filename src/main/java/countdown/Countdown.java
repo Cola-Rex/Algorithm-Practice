a0fc6cbb866d847f1fbff6ae3db78afcedcbcc2b
Ezreal <1649811141@qq.com>
2018-10-08 15:45:19 +0800

    2个倒计时的实现
    1，单线程的比较稳；
    2，线程池的实现有点虚，时不时给你弄到 -1 秒去了，慎用。

---
+++ src/main/java/countdown/Countdown.java
@@ -0,0 +1,32 @@
+package countdown;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 倒计时 单线程 实现
+ */
+public class Countdown {
+
+	private int limitSec;
+	
+	public Countdown(int limitSec) {
+		this.limitSec = limitSec;
+	}
+	
+	private void countdown() throws InterruptedException {
+		System.out.println("倒计时： " + limitSec + "秒开始");
+		while (limitSec > 0) {
+			TimeUnit.SECONDS.sleep(1); //休息1秒
+			System.out.println("剩余： " + --limitSec + "秒" );
+		}
+		System.out.println("午时已到");
+	}
+	
+	public static void main(String[] args) {
+		try {
+			new Countdown(5).countdown();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
