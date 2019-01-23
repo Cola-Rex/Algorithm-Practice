a406679549bd03341b1f08a95ac71f6ddc08078f
Ezreal <1649811141@qq.com>
2019-01-23 22:31:51 +0800

    修改类名

--- src/main/java/binary/binarySearch/Recursion.java
+++ src/main/java/binary/binarySearch/Recursion.java
@@ -1,13 +1,19 @@
 package binary.binarySearch;
 
-public class BinarySearch {
+/**
+ * 递归法
+ */
+public class Recursion {
+	
     public int searchInsert(int[] nums, int target) {
         return binarySearch(nums, target, 0, nums.length-1);
     }
     
     public int binarySearch(int[] nums, int target, int left, int right) {
-        int mid = (left + right) / 2;
         
+    	// 避免这种写法int mid = (left + right) / 2; 可能left+right越界
+        int mid = left + (right - left) / 2;
+    	
         if(left >= right) {
             if(nums[mid] < target) return mid + 1;
             else return mid;
